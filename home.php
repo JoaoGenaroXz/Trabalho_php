502de80e3f88ac4edb48e2f0c0d0d3bbfe4e0421
João Victor <45217068+JoaoGenaroXz@users.noreply.github.com>
2022-11-19 00:42:52 -0300

    Add files via upload

---
+++ home.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <title>Home</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<nav class="navbar navbar-expand-sm fixed-top">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="XZone_SemFundo.png" alt="Sistema" style="width:150px;" class="rounded-pill"> 
+        </a>
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Cadastro</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="cadastroCompromisso.php" button = "cadastroCompromisso.php">Compromisso</a></li>
+                            <li><a class="dropdown-item" href="#" button = "" >Vago</a></li>
+                            <li><a class="dropdown-item" href="#"button = "">Vago</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Listagem</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#" button = "listarCompromisso.php">Compromisso</a></li>
+                            <li><a class="dropdown-item" href="#">Vago</a></li>
+                            <li><a class="dropdown-item" href="#">Vago</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="sair.php">Sair</a>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
+<link rel="stylesheet" href="home.css">
+<body>
+    <div class = "ini">
+
+    </div>
+    
+
+</body>
+</html>
